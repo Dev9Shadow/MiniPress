c0e7b30127e3c0b88012d034f40fea7bb5417545
Romain Santilli <romain.santilli3@etu.univ-lorraine.fr>
2026-06-10 15:45:19 +0200

    creation de l'action lister les articles

---
+++ minipress.core/minipress.appli/src/controlers/api/ListerArticlesAction.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace minipress\appli\controlers\api;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use minipress\appli\services\ArticleService;
+use minipress\appli\services\ArticleServiceInterface;
+
+final class ListerArticlesAction
+{
+    private ArticleServiceInterface $service;
+
+    public function __construct()
+    {
+        $this->service = new ArticleService();
+    }
+
+    public function __invoke(Request $rq, Response $rs, array $args): Response
+    {
+        $articles = $this->service->listerArticlesPublies();
+        $rs->getBody()->write(json_encode($articles));
+        return $rs->withHeader('Content-Type', 'application/json');
+    }
+}
