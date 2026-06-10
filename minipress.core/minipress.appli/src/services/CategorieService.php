2c2107bcc7bef9808fee92f7455cabd0d71d3b24
Romain Santilli <romain.santilli3@etu.univ-lorraine.fr>
2026-06-10 13:58:20 +0200

    ajout de la requete eloquent pour recuperer toutes les categories

--- minipress.core/minipress.appli/src/services/CategorieService.php
+++ minipress.core/minipress.appli/src/services/CategorieService.php
@@ -3,11 +3,19 @@ declare(strict_types=1);
 
 namespace minipress\appli\services;
 
+use minipress\appli\models\Categorie;
+use minipress\appli\services\exceptions\InternalErrorException;
+use Illuminate\Database\QueryException;
+
 class CategorieService implements CategorieServiceInterface
 {
     public function listerCategories(): array
     {
-        return [];
+        return Categorie::orderBy('libelle')->get()->map(fn($c) => [
+            'id'      => $c->id,
+            'libelle' => $c->libelle,
+            'href'    => '/api/categories/' . $c->id,
+        ])->all();
     }
 
     public function creerCategorie(array $data): int
