f6fc3489fe1946af2021d63a5f2a3fa672245880
Romain Santilli <romain.santilli3@etu.univ-lorraine.fr>
2026-06-10 14:55:40 +0200

    creation de la classe ArticleService avec listerArticlesPublies

---
+++ minipress.core/minipress.appli/src/services/ArticleService.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace minipress\appli\services;
+
+use minipress\appli\models\Article;
+use minipress\appli\services\exceptions\EntityNotFoundException;
+use minipress\appli\services\exceptions\InternalErrorException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+
+class ArticleService implements ArticleServiceInterface
+{
+    public function listerArticlesPublies(?string $tri = null): array
+    {
+        return [];
+    }
+
+    public function listerArticlesParCategorie(int $categorieId, ?string $tri = null): array
+    {
+        return [];
+    }
+
+    public function listerArticlesParAuteur(int $auteurId, ?string $tri = null): array
+    {
+        return [];
+    }
+
+    public function afficherArticle(int $id): array
+    {
+        return [];
+    }
+
+    public function listerTousArticles(): array
+    {
+        return [];
+    }
+
+    public function creerArticle(array $data, int $auteurId): int
+    {
+        return 0;
+    }
+
+    public function basculerPublication(int $id): void {}
+}
