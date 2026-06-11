3407eaf934352180527950a5dff56217c7524ca0
Romain Santilli <romain.santilli3@etu.univ-lorraine.fr>
2026-06-11 09:40:50 +0200

    route pour afficher un seul article complet

--- minipress.core/minipress.appli/public/api.php
+++ minipress.core/minipress.appli/public/api.php
@@ -8,6 +8,7 @@ use minipress\appli\utils\Eloquent;
 use minipress\appli\controlers\api\ListerCategoriesAction;
 use minipress\appli\controlers\api\ListerArticlesAction;
 use minipress\appli\controlers\api\ListerArticlesCategorieAction;
+use minipress\appli\controlers\api\AfficherArticleAction;
 
 Eloquent::init(__DIR__ . '/../src/conf/minipress.db.conf.ini');
 
@@ -18,5 +19,6 @@ $app->addErrorMiddleware(true, true, true);
 $app->get('/api/categories', ListerCategoriesAction::class);
 $app->get('/api/articles', ListerArticlesAction::class);
 $app->get('/api/categories/{id_categ}/articles', ListerArticlesCategorieAction::class);
+$app->get('/api/articles/{id_a}', AfficherArticleAction::class);
 
 $app->run();
