49869deec65e0572df50fdb76dd80fc5b2451b3c
Erdal <erdal@iutnc.fr>
2026-06-16 17:12:33 +0200

    feat: ApiService complet (article, categorie, auteur)

--- minipress.app/lib/services/api_service.dart
+++ minipress.app/lib/services/api_service.dart
@@ -1,22 +1,35 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../models/article.dart';
 import '../models/article_resume.dart';
 import '../models/categorie.dart';
 
 const String _base = 'http://docketu.iutnc.univ-lorraine.fr:57000/api';
 
 class ApiService {
-  static Future<List<Categorie>> fetchCategories() async {
-    final res = await http.get(Uri.parse('$_base/categories'));
+  static Future<T> _get<T>(String url, T Function(dynamic) parse) async {
+    final res = await http.get(Uri.parse(url));
     if (res.statusCode != 200) throw Exception('Erreur HTTP ${res.statusCode}');
-    final List json = jsonDecode(res.body);
-    return json.map((e) => Categorie.fromJson(e)).toList();
+    return parse(jsonDecode(res.body));
   }
 
-  static Future<List<ArticleResume>> fetchArticles({String sort = 'date-desc'}) async {
-    final res = await http.get(Uri.parse('$_base/articles?sort=$sort'));
-    if (res.statusCode != 200) throw Exception('Erreur HTTP ${res.statusCode}');
-    final List json = jsonDecode(res.body);
-    return json.map((e) => ArticleResume.fromJson(e)).toList();
-  }
+  static Future<List<Categorie>> fetchCategories() => _get(
+      '$_base/categories',
+      (j) => (j as List).map((e) => Categorie.fromJson(e)).toList());
+
+  static Future<List<ArticleResume>> fetchArticles({String sort = 'date-desc'}) => _get(
+      '$_base/articles?sort=$sort',
+      (j) => (j as List).map((e) => ArticleResume.fromJson(e)).toList());
+
+  static Future<Article> fetchArticle(int id) =>
+      _get('$_base/articles/$id', (j) => Article.fromJson(j));
+
+  static Future<List<ArticleResume>> fetchArticlesByCategorie(int id,
+          {String sort = 'date-desc'}) =>
+      _get('$_base/categories/$id/articles?sort=$sort',
+          (j) => (j as List).map((e) => ArticleResume.fromJson(e)).toList());
+
+  static Future<List<ArticleResume>> fetchArticlesByAuteur(int id) => _get(
+      '$_base/auteurs/$id/articles',
+      (j) => (j as List).map((e) => ArticleResume.fromJson(e)).toList());
 }
